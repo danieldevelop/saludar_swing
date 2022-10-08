a07f373f5bdb269e19b5d188bb1b58a8501d5c01
Daniel Gomez <dgomez.informatica@gmail.com>
2022-10-08 02:42:58 -0300

    Clase principal donde se llama al formulario FormSaludar

---
+++ src/com/swing/Saludar_Swing.java
@@ -0,0 +1,12 @@
+package com.swing;
+
+import com.swing.jframe.FormSaludar;
+
+public class Saludar_Swing {
+
+	public static void main(String[] args) {
+		FormSaludar form = new FormSaludar();
+		form.setVisible(true);
+	}
+
+}
